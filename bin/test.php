40d99a4d7aab3bdcad423389a90b6239d7571a1d
Björn Dieding <bjoern@xrow.de>
2013-07-04 15:26:13 +0200

    - changes

---
+++ bin/test.php
@@ -0,0 +1,33 @@
+<?php
+
+use XROW\Salesforce\Salesforce;
+
+$connection = Salesforce::factory();
+
+//Describing the Leads object and printing the array
+$describe = $connection->describeSObjects(array('Lead'));
+file_put_contents( "sf.test.txt", print_r( $describe, true ));
+
+
+//Creating the Lead Object
+$lead = new stdClass;
+$lead->FirstName = "Björn";
+$lead->LastName = "Dieding";
+$lead->Company = "xrow GmbH";
+$lead->Email = "bjoern@fake.xrow.net";
+$result = $connection->create(array($lead), 'Lead');
+
+
+//Creating the Lead Object
+$leadmember = new stdClass;
+$leadmember->CampaignId = "701J0000000JfZf";
+$leadmember->LeadId = $result[0]->id;
+
+$result = $connection->create(array($leadmember), 'CampaignMember');
+
+
+var_dump($result);
+
+#CampaignMember
+//$result = $connection->convertLead($lead);
+//Submitting the Lead to Salesforce
---
+++ classes/Salesforce.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace XROW\Salesforce;
+
+use eZINI;
+use SforceEnterpriseClient;
+
+class Salesforce
+{
+    private static $connection = null;
+    private static $location = null;
+    private static $session = null;
+
+    /**
+     * return SforceBaseClient
+     */
+    public static function factory()
+    {
+        if ( self::$connection )
+        {
+            return self::$connection;
+        }
+        $ini = eZINI::instance( "salesforce.ini" );
+        
+        $file = "extension/xrowsalesforce/share/salesforce.enterprise.wsdl.xml";
+        
+        /*
+         * $file = eZSys::cacheDirectory() . "/salesforce.enterprise.wsdl.xml"; if (!file_exists($file)) { file_put_contents( $file, file_get_contents( "https://cs10.salesforce.com/soap/wsdl.jsp?type=*" ) ); echo "$file"; }
+         */
+        self::$connection = new SforceEnterpriseClient();
+        self::$connection->createConnection( $file );
+        
+        if ( self::$session and self::$location )
+        {
+            
+            self::$connection->setEndpoint( self::$location );
+            self::$connection->setSessionHeader( self::$session );
+        }
+        else
+        {
+            self::$connection->login( $ini->variable( 'Settings', 'Username' ), $ini->variable( 'Settings', 'Password' ) . $ini->variable( 'Settings', 'Token' ) );
+            self::$location = self::$connection->getLocation();
+            self::$session = self::$connection->getSessionId();
+        }
+        return self::$connection;
+    }
+}
