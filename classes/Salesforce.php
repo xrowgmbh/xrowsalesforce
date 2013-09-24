bc61d0c5a408ffe89b5e8a0ef18bcefa0ed92a42
kebel <kebel@wuvtest-admin-1.hst.sv-www.de>
2013-09-24 12:37:18 +0200

    load settings block for live or test server

--- classes/Salesforce.php
+++ classes/Salesforce.php
@@ -21,27 +21,31 @@ class Salesforce
             return self::$connection;
         }
         $ini = eZINI::instance( "salesforce.ini" );
-
-        $file = "extension/xrowsalesforce/share/salesforce.enterprise.wsdl.xml";
-
-        /*
-         * $file = eZSys::cacheDirectory() . "/salesforce.enterprise.wsdl.xml"; if (!file_exists($file)) { file_put_contents( $file, file_get_contents( "https://cs10.salesforce.com/soap/wsdl.jsp?type=*" ) ); echo "$file"; }
-         */
-        self::$connection = new SforceEnterpriseClient();
-        self::$connection->createConnection( $file );
-
-        if ( self::$session and self::$location )
+        $loadBlock = $ini->variable( 'Settings', 'LoadBlock' );
+        $dataBlock = $ini->BlockValues[$loadBlock];
+        if( file_exists( $dataBlock['File'] ) )
         {
-            
-            self::$connection->setEndpoint( self::$location );
-            self::$connection->setSessionHeader( self::$session );
+            self::$connection = new SforceEnterpriseClient();
+            self::$connection->createConnection( $dataBlock['File'] );
+
+            if ( self::$session and self::$location )
+            {
+                
+                self::$connection->setEndpoint( self::$location );
+                self::$connection->setSessionHeader( self::$session );
+            }
+            else
+            {
+                self::$connection->login( $dataBlock['Username'], $dataBlock['Password'] . $dataBlock['Token'] );
+                self::$location = self::$connection->getLocation();
+                self::$session = self::$connection->getSessionId();
+            }
+            return self::$connection;
         }
         else
         {
-            self::$connection->login( $ini->variable( 'Settings', 'Username' ), $ini->variable( 'Settings', 'Password' ) . $ini->variable( 'Settings', 'Token' ) );
-            self::$location = self::$connection->getLocation();
-            self::$session = self::$connection->getSessionId();
+            eZDebug::writeError( $dataBlock['File'] . ' does not exist.', __METHOD__ );
+            return false;
         }
-        return self::$connection;
     }
 }
--- settings/salesforce.ini.append.php
+++ settings/salesforce.ini.append.php
@@ -1,8 +1,23 @@
 <?php /* #?ini charset="utf-8"?
 
 [Settings]
-#Username=
+#LoadBlock=SalesForceData_Test
+
+#ExportFieldName[]
+#ExportFieldName[e_mail_adresse]=Email
+#ExportFieldName[vorname]=FirstName
+#ExportFieldName[nachname]=LastName
+
+#[SalesForceData_Test]
+#Username=xy@z.sandbox
 #Password=
 #Token=
+#File=extension/xyz/share/salesforceTEST.enterprise.wsdl.xml
+
+#[SalesForceData_Live]
+#Username=xy@z
+#Password=
+#Token=
+#File=extension/xyz/share/salesforce.enterprise.wsdl.xml
 
 */ ?>
